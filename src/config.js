8cfb3123c34721a3ce52b156b9fc63603c886378
Paul Han <kyutae.han93@gmail.com>
2019-10-08 20:02:33 -0400

    fixed up favicon and background image scaling when the verticalness of the page kept growing

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api'||'https://stark-atoll-11428.herokuapp.com/api',
+  API_ENDPOINT: /* 'http://localhost:8000/api'|| */'https://stark-atoll-11428.herokuapp.com/api',
   TOKEN_KEY: 'tftBuilds-client-auth-token',
 }
